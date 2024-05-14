52ae14b1542f6815ea12fdaba55a58901c3f4599
ramenkiller <162812787+ramenkiller@users.noreply.github.com>
2024-05-14 19:15:41 +0900

    add CORS and React test

---
+++ src/main/java/com/codingchosun/backend/controller/reactTestController.java
@@ -0,0 +1,22 @@
+package com.codingchosun.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class reactTestController {
+
+    @PostMapping
+    public String postTest(@RequestBody String body) {
+        log.info("postBody: " + body);
+
+        return "ok";
+    }
+}
--- src/main/java/com/codingchosun/backend/web/WebConfig.java
+++ src/main/java/com/codingchosun/backend/web/WebConfig.java
@@ -5,6 +5,7 @@ import com.codingchosun.backend.web.argumentresolver.LoginUserArgumentResolver;
 import com.codingchosun.backend.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,18 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    //cors설정은 localhost:3000에게 API로의 접근을 허용해준다.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
