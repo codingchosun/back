19efd999f725b4a0020a23fd04bc044f19da0fdb
gimzaezin <kimjaejin@chosun.kr>
2024-05-27 17:25:33 +0900

    Update Login param -> body

--- src/main/java/com/codingchosun/backend/request/LoginRequest.java
+++ src/main/java/com/codingchosun/backend/request/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.codingchosun.backend.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,10 @@ import lombok.Setter;
 @Setter
 public class LoginRequest {
     @NotEmpty
+    @JsonProperty("loginId")
     private String loginId;
 
     @NotEmpty
+    @JsonProperty("password")
     private String password;
 }
