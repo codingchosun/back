f1a56dc37c94dfa0951e516d023210a5775b9fb8
gimzaezin <kimjaejin@chosun.kr>
2024-05-28 11:15:29 +0900

    Mypost 컨트롤러 서비스 테스트 수정 profiletest.sql 테스트 sql 작성

--- src/main/java/com/codingchosun/backend/controller/MypostController.java
+++ src/main/java/com/codingchosun/backend/controller/MypostController.java
@@ -5,6 +5,8 @@ import com.codingchosun.backend.response.ApiResponse;
 import com.codingchosun.backend.response.MyPostResponse;
 import com.codingchosun.backend.service.MyPostService;
 import com.codingchosun.backend.web.argumentresolver.Login;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class MypostController {
 
     MyPostService myPostService;
 
+    @Autowired
+    public MypostController(MyPostService myPostService) {
+        this.myPostService = myPostService;
+    }
+
     @GetMapping("/mypost")
     public ApiResponse<List<MyPostResponse>> mypost(@Login User user, Model model) {
+        log.info("login user:{}", user);
         List<MyPostResponse> responseList = myPostService.getMyPost(user);
         return new ApiResponse<>(HttpStatus.OK, true, responseList);
     }
