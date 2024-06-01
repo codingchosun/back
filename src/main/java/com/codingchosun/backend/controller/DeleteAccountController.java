3dc7f6255aef8379d21f6bec0d33b6dfa5c63508
gimzaezin <kimjaejin@chosun.kr>
2024-06-01 17:11:07 +0900

    Create DeleteAccount 회원탈퇴기능구현

---
+++ src/main/java/com/codingchosun/backend/controller/DeleteAccountController.java
@@ -0,0 +1,27 @@
+package com.codingchosun.backend.controller;
+
+import com.codingchosun.backend.domain.User;
+import com.codingchosun.backend.response.ApiResponse;
+import com.codingchosun.backend.service.DeleteAccountService;
+import com.codingchosun.backend.web.argumentresolver.Login;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeleteAccountController {
+
+    DeleteAccountService deleteAccountService;
+
+    @Autowired
+    public DeleteAccountController(DeleteAccountService deleteAccountService) {
+        this.deleteAccountService = deleteAccountService;
+    }
+
+    @PostMapping("/deleteAccount")
+    public ApiResponse<String> deleteAccount(@Login User user) {
+        deleteAccountService.deleteAccount(user);
+        return new ApiResponse<>(HttpStatus.ACCEPTED, true, "회원탈퇴 성공");
+    }
+}
