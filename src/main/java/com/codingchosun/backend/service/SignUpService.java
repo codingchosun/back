239e594453e360a33a3e9240a6a86d10cbf714ab
gimzaezin <kimjaejin@chosun.kr>
2024-06-02 19:52:45 +0900

    Rename register->signUP

--- src/main/java/com/codingchosun/backend/service/SignUpService.java
+++ src/main/java/com/codingchosun/backend/service/SignUpService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class RegisterService {
+public class SignUpService {
 
     private final DataJpaUserRepository dataJpaUserRepository;
 
