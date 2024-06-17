44e2c2fcca7772ed339f9f8374c3db1d833f6d71
gimzaezin <kimjaejin@chosun.kr>
2024-06-17 23:37:22 +0900

    hihi

--- src/main/java/com/codingchosun/backend/response/UserDTO.java
+++ src/main/java/com/codingchosun/backend/response/UserDTO.java
@@ -8,9 +8,11 @@ public class UserDTO {
 
     private Long userId;
     private String nickname;
+    private String loginId;
 
     public UserDTO(User user){
         this.userId = user.getUserId();
+        this.loginId = user.getLoginId();
         this.nickname = user.getNickname();
     }
 
