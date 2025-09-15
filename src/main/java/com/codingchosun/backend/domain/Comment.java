94d3ee999737b2aefa22c24edc6398360a86a012
sugwan <skhrnt2945@gmail.com>
2025-09-16 00:39:30 +0900

    fix: @JoinColumn(name 속성) 제거

--- src/main/java/com/codingchosun/backend/domain/Comment.java
+++ src/main/java/com/codingchosun/backend/domain/Comment.java
@@ -10,23 +10,21 @@ import java.time.LocalDateTime;
 @Getter @Setter
 @Table
 public class Comment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
 
     private String content;
 
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId")
+    @JoinColumn
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "postId")
+    @JoinColumn
     private Post post;
-
 
 }
