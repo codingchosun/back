627669aae6f07fcd511fa5ecdb84c069c486c938
sugwan <skhrnt2945@gmail.com>
2025-09-16 00:08:58 +0900

    fix: Post 테이블의 불필요한 @Temporal, @JoinColumn(name 속성) 제거

--- src/main/java/com/codingchosun/backend/domain/Post.java
+++ src/main/java/com/codingchosun/backend/domain/Post.java
@@ -5,14 +5,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Check;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
-//@Check(constraints = "start_time >= created_at AND end_time >= created_at")
-//예외 조건 추가할지 이야기 할것
-//사유: 추가하면 귀찮아질듯
 
 @Entity
 @Getter @Setter
@@ -27,19 +22,16 @@ public class Post {
 
     private String content;
 
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime startTime;
 
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime endTime;
 
     @Enumerated(EnumType.STRING)
     private StateCode stateCode;
 
-    private  Long viewCount;
+    private Long viewCount;
 
     @OneToMany(mappedBy = "post")
     private List<PostHash> postHashes;
@@ -48,17 +40,17 @@ public class Post {
     private List<PostUser> postUsers;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId")
+    @JoinColumn
     private User user;
 
     @OneToMany(mappedBy = "post")
     private List<Comment> comments;
+
     @OneToMany(mappedBy = "post")
     private List<Image> images;
 
     @OneToMany(mappedBy = "post")
     private List<Validate> validates;
-
 
     public void increaseViewCount() {
         this.viewCount++;
