d4afd20a2434e02d1030b577993e3e2cdc254c9c
sugwan <skhrnt2945@gmail.com>
2025-09-18 15:44:41 +0900

    refactor: Template 리포지토리단 이름 통일
    TemplateRepository -> DataJpaTemplateRepository

--- src/main/java/com/codingchosun/backend/repository/template/DataJpaTemplateRepository.java
+++ src/main/java/com/codingchosun/backend/repository/template/DataJpaTemplateRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface TemplateRepository extends JpaRepository<Template, Long> {
+public interface DataJpaTemplateRepository extends JpaRepository<Template, Long> {
 
     Optional<Template> findTemplateByContent(String content);
 
-    List<Template> findByValidates_ToUser_UserId(Long id);
+    List<Template> findByEvaluations_ToUser_UserId(Long evaluationsToUserUserId);
 
 }
