86b3ba00853e3d7325a8192e73fef79fed3baf66
sugwan <skhrnt2945@gmail.com>
2025-09-18 23:58:12 +0900

    remove: 사용하지 않거나 이름이 잘못된 예외 제거

--- src/main/java/com/codingchosun/backend/exception/FileException.java
+++
@@ -1,7 +0,0 @@
-package com.codingchosun.backend.exception;
-
-public class FileException extends RuntimeException {
-    public FileException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/codingchosun/backend/exception/PasswordNotMatch.java
+++
@@ -1,7 +0,0 @@
-package com.codingchosun.backend.exception;
-
-public class PasswordNotMatch extends RuntimeException {
-    public PasswordNotMatch(String message) {
-        super(message);
-    }
-}
