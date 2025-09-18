d7a2dc729b9786ed86fdfcc88bd93703a24c9f61
sugwan <skhrnt2945@gmail.com>
2025-09-18 11:49:12 +0900

    fix: ImageService 의 조회 서비스를 ImageQueryService 로 분리 및 수정

--- src/main/java/com/codingchosun/backend/service/ImageService.java
+++ src/main/java/com/codingchosun/backend/service/ImageService.java
@@ -1,86 +1,78 @@
 package com.codingchosun.backend.service;
 
 import com.codingchosun.backend.component.file.FileStore;
+import com.codingchosun.backend.component.file.UploadFile;
 import com.codingchosun.backend.domain.Image;
 import com.codingchosun.backend.domain.Post;
 import com.codingchosun.backend.domain.User;
-import com.codingchosun.backend.exception.invalidrequest.IsNotPostAuthor;
-import com.codingchosun.backend.exception.invalidrequest.IsNotPostImage;
+import com.codingchosun.backend.exception.FileException;
+import com.codingchosun.backend.exception.notfoundfromdb.EntityNotFoundFromDB;
 import com.codingchosun.backend.exception.notfoundfromdb.ImageNotFoundFromDB;
-import com.codingchosun.backend.repository.image.DataJpaImageRepository;
-import com.codingchosun.backend.response.ImageResponse;
-import jakarta.transaction.Transactional;
+import com.codingchosun.backend.exception.notfoundfromdb.PostNotFoundFromDB;
+import com.codingchosun.backend.repository.post.DataJpaPostRepository;
+import com.codingchosun.backend.repository.user.DataJpaUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
-@Slf4j
 @RequiredArgsConstructor
 public class ImageService {
 
-    private final DataJpaImageRepository dataJpaImageRepository;
+    private final DataJpaPostRepository postRepository;
+    private final DataJpaUserRepository userRepository;
     private final FileStore fileStore;
 
-    public Page<ImageResponse> getImageURLList(Pageable pageable, Long postId){
-        return dataJpaImageRepository.findByPost_PostId(postId, pageable).map(ImageResponse::new);
-    }
-
-    //여러개의 파일을 받아서 지정된 경로에 저장, 리턴값으로는 저장한 파일개수 반환
-    public int uploadImages(List<MultipartFile> multipartFiles, Post post) {
-
-        List<String> savedFiles;    //파일의 본래 이름이 아닌 저장 됐을때의 이름
-        int count = 0;              //저장한 파일 개수
-
+    //여러개의 파일을 받아서 지정된 경로에 저장
+    public void uploadImages(Long postId, String loginId, List<MultipartFile> multipartFiles) {
         try {
-            savedFiles = fileStore.storeImages(multipartFiles);
+            User user = findUserByLoginId(loginId);
+            Post post = findPostById(postId);
+            post.validateOwner(user);
+
+            List<UploadFile> uploadFiles = fileStore.storeImages(multipartFiles);
+
+            for (UploadFile file : uploadFiles) {
+                Image image = new Image(file.getStoreFileName(), file.getUploadFileName(), post);
+                post.addImage(image);
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e);  //파일 저장 중 실패
+            throw new FileException("이미지 저장 실패");
         }
-
-        if(savedFiles.isEmpty()){
-            log.info("사진 없음");  //MultipartFile이 비었을 경우
-        }
-
-        for (String savedFile : savedFiles) {
-            Image image = new Image();
-            image.setPost(post);
-            image.setUrl(savedFile);
-            image.setName("none");    //일단 생략
-
-            dataJpaImageRepository.save(image); //저장
-            count++;
-        }
-
-        return  count;
     }
 
-    public Long deleteImage(User user, Post post, Long imageId) {
-        Image image = dataJpaImageRepository.findById(imageId)
-                .orElseThrow(() -> new ImageNotFoundFromDB("image id = " + imageId + " 못 찾음"));
+    //이미지 삭제
+    public void deleteImage(Long postId, Long imageId, String loginId) {
+        User user = findUserByLoginId(loginId);
+        Post post = findPostById(postId);
+        post.validateOwner(user);
 
-        Long targetId = image.getImageId();
+        boolean removed = post.getImages()
+                .removeIf(image -> image.getImageId()
+                        .equals(imageId)
+                );
 
-        //글쓴이 확인
-        if( !(post.getUser().getUserId().equals(user.getUserId())) ){
-            throw new IsNotPostAuthor("글쓴이만 삭제할수있습니다.");
+        if (!removed) {
+            throw new ImageNotFoundFromDB("해당 게시물에 존재하지 않는 이미지입니다: " + imageId);
         }
-        //post의 이미지가 맞는지 확인
-        if( !(post.getImages().contains(image)) ){
-            throw new IsNotPostImage("imageId:" + targetId + "는 postId:" + post.getPostId() + "의 이미지가 아닙니다");
-        }
-
-        dataJpaImageRepository.delete(image);
-
-        return targetId;
     }
 
+    private Post findPostById(Long postId) {
+        return postRepository.findById(postId).orElseThrow(
+                () -> new PostNotFoundFromDB("해당 게시물을 찾지 못하였습니다" + postId)
+        );
+    }
 
+    private User findUserByLoginId(String loginId) {
+        return userRepository.findByLoginId(loginId).orElseThrow(
+                () -> new EntityNotFoundFromDB("아이디에 해당하는 유저를 찾지 못했습니다" + loginId)
+        );
+    }
 }
