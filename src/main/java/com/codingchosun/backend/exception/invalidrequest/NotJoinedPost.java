97a21737297e3611c569d96dfa8ef9243a3eb853
sugwan <skhrnt2945@gmail.com>
2025-09-18 23:58:28 +0900

    remove: 사용하지 않거나 이름이 잘못된 예외 제거2

--- src/main/java/com/codingchosun/backend/exception/invalidrequest/NotJoinedPost.java
+++
@@ -1,7 +0,0 @@
-package com.codingchosun.backend.exception.invalidrequest;
-
-public class NotJoinedPost extends RuntimeException{
-    public NotJoinedPost(String message) {
-        super(message);
-    }
-}
