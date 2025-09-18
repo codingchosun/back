d0720613005ffac222b2512c746982892d8d86a8
sugwan <skhrnt2945@gmail.com>
2025-09-19 00:05:15 +0900

    refactor: 예외 이름을 직관적인 이름으로 변경

--- src/main/java/com/codingchosun/backend/exception/invalidrequest/IsNotPostAuthor.java
+++
@@ -1,7 +0,0 @@
-package com.codingchosun.backend.exception.invalidrequest;
-
-public class IsNotPostAuthor extends RuntimeException{
-    public IsNotPostAuthor(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/codingchosun/backend/exception/invalidrequest/YouAreNotAdmin.java
+++
@@ -1,7 +0,0 @@
-package com.codingchosun.backend.exception.invalidrequest;
-
-public class YouAreNotAdmin extends RuntimeException{
-    public YouAreNotAdmin(String message) {
-        super(message);
-    }
-}
