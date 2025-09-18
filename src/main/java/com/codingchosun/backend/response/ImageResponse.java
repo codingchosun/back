8431e3a520e8c2630053b72a4247c5376b41f9fb
sugwan <skhrnt2945@gmail.com>
2025-09-18 11:49:54 +0900

    refactor: 파일저장 응답 DTO 및 FileStore 코드 리팩토링

--- src/main/java/com/codingchosun/backend/response/ImageResponse.java
+++ src/main/java/com/codingchosun/backend/response/ImageResponse.java
@@ -1,22 +1,38 @@
 package com.codingchosun.backend.response;
 
 import com.codingchosun.backend.domain.Image;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageResponse {
+
     private Long imageId;
+    private String uploadFileName;
     private String url;
 
-    public ImageResponse(Image image){
-        this.imageId = image.getImageId();
+    private static String urlParsing(String url) {
+        if (url != null) {
+            String[] urlSplit = url.split("/");
+            url = urlSplit[urlSplit.length - 1];
+        }
+        return url;
+    }
 
-        if(image.getUrl() != null){
-            String[] split = image.getUrl().split("/");
-            this.url = split[split.length - 1];
-        }
-        else{
-            this.url = "";
-        }
+    public static ImageResponse from(Image image) {
+        return new ImageResponse(
+                image.getImageId(),
+                image.getName(),
+                urlParsing(image.getUrl())
+        );
+    }
+
+    public ImageResponse(Image image) {
+        this.imageId = image.getImageId();
+        this.uploadFileName = image.getName();
+        this.url = image.getUrl();
     }
 }
