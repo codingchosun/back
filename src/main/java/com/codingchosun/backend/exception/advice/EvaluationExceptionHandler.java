db8bb0fc51deccd6cf2cbdf9f447ddea85b6852d
sugwan <skhrnt2945@gmail.com>
2025-09-19 00:06:50 +0900

    feat: 구분용 패키지 추가

---
+++ src/main/java/com/codingchosun/backend/exception/advice/EvaluationExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.codingchosun.backend.exception.advice;
+
+import com.codingchosun.backend.controller.evaluation.EvaluationController;
+import com.codingchosun.backend.dto.response.ApiResponse;
+import com.codingchosun.backend.dto.response.ErrorResponse;
+import com.codingchosun.backend.exception.common.ErrorCode;
+import com.codingchosun.backend.exception.invalidrequest.AlreadyEvaluatedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackageClasses = {EvaluationController.class})
+public class EvaluationExceptionHandler {
+
+    @ExceptionHandler(AlreadyEvaluatedException.class)
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleAlreadyEvaluated(AlreadyEvaluatedException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.warn("평가 예외: {}", errorCode.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+
+        return new ResponseEntity<>(new ApiResponse<>(errorCode.getStatus(), false, errorResponse), errorCode.getStatus());
+    }
+}
---
+++ src/main/java/com/codingchosun/backend/exception/advice/ImageExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.codingchosun.backend.exception.advice;
+
+import com.codingchosun.backend.controller.image.ImageController;
+import com.codingchosun.backend.dto.response.ApiResponse;
+import com.codingchosun.backend.dto.response.ErrorResponse;
+import com.codingchosun.backend.exception.common.ErrorCode;
+import com.codingchosun.backend.exception.invalidrequest.NotPostImageException;
+import com.codingchosun.backend.exception.invalidrequest.UnauthorizedActionException;
+import com.codingchosun.backend.exception.notfoundfromdb.ImageNotFoundFromDB;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackageClasses = ImageController.class)
+public class ImageExceptionHandler {
+
+    @ExceptionHandler(UnauthorizedActionException.class)
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleUnauthorizedAction(UnauthorizedActionException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.warn("Image 작업 권한 예외: {}", errorCode.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+        return new ResponseEntity<>(new ApiResponse<>(errorCode.getStatus(), false, errorResponse), errorCode.getStatus());
+    }
+
+    @ExceptionHandler(ImageNotFoundFromDB.class)
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleImageNotFound(ImageNotFoundFromDB e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.warn("Image 엔티티 예외: {}", errorCode.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+        return new ResponseEntity<>(new ApiResponse<>(errorCode.getStatus(), false, errorResponse), errorCode.getStatus());
+    }
+
+    @ExceptionHandler(NotPostImageException.class)
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleNotPostImage(NotPostImageException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.warn("Image 게시글 예외: {}", errorCode.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+        return new ResponseEntity<>(new ApiResponse<>(errorCode.getStatus(), false, errorResponse), errorCode.getStatus());
+    }
+
+}
