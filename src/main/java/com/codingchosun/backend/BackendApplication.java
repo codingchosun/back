0a93110551b4fd0b5eb283fcd045ed9083aecc80
sugwan <skhrnt2945@gmail.com>
2025-09-20 12:37:50 +0900

    docs: 타임존 설정 추가

--- src/main/java/com/codingchosun/backend/BackendApplication.java
+++ src/main/java/com/codingchosun/backend/BackendApplication.java
@@ -1,12 +1,24 @@
 package com.codingchosun.backend;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Date;
+import java.util.TimeZone;
+
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("현재 시각: {}", new Date());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
