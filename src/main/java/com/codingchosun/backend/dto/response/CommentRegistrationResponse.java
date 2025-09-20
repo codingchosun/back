fb62531a4053e09e07f1b5704b847d44df45f830
sugwan <skhrnt2945@gmail.com>
2025-09-20 13:04:32 +0900

    refactor: 댓글 API 응답값 형식 통일 및 CommentService 의 삭제 검증 코드 추가

---
+++ src/main/java/com/codingchosun/backend/dto/response/CommentRegistrationResponse.java
@@ -0,0 +1,12 @@
+package com.codingchosun.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommentRegistrationResponse {
+
+    private final Long commentId;
+
+}
